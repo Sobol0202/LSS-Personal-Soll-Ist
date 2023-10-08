54cd974ad4c388a44d55cf1a4e170e38731d8399
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-10-08 15:07:37 +0200

    Update LSS Personal Soll-Ist.user.js
    +Added Freitext funktion to Building interface
    +Added Freitext funktion to tabelle interface

--- LSS Personal Soll-Ist.user.js
+++ LSS Personal Soll-Ist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Personal Soll-Ist
 // @namespace    www.leitstellenspiel.de
-// @version      1.1
+// @version      1.2
 // @description  Fügt Personalverwaltungsfunktionen hinzu
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/buildings/*
@@ -22,6 +22,17 @@
         localStorage.setItem('personalData', JSON.stringify(data));
     }
 
+    // Funktion zum Lesen von Textdaten aus dem lokalen Speicher
+    function getLocalStorageTextData() {
+        const data = localStorage.getItem('personalTextData');
+        return data ? JSON.parse(data) : {};
+    }
+
+    // Funktion zum Schreiben von Textdaten in den lokalen Speicher
+    function setLocalStorageTextData(data) {
+        localStorage.setItem('personalTextData', JSON.stringify(data));
+    }
+
     // Funktion zur Aktualisierung des Eingabefelds
     function updateInputField(buildingId) {
         const personalData = getLocalStorageData();
@@ -29,11 +40,25 @@
         inputField.value = personalCount;
     }
 
+    // Funktion zur Aktualisierung des Eingabefelds für Freitext
+    function updateTextInputField(buildingId) {
+        const textData = getLocalStorageTextData();
+        const textValue = textData[buildingId] || '';
+        textInputField.value = textValue;
+    }
+
     // Funktion zur Aktualisierung der Personalanzahl
     function updatePersonalCount(buildingId, count) {
         const personalData = getLocalStorageData();
         personalData[buildingId] = count;
         setLocalStorageData(personalData);
+    }
+
+    // Funktion zum Aktualisieren des Freitexts
+    function updateTextValue(buildingId, text) {
+        const textData = getLocalStorageTextData();
+        textData[buildingId] = text;
+        setLocalStorageTextData(textData);
     }
 
     // Funktion zum Löschen der Personalanzahl
@@ -44,10 +69,19 @@
         updateInputField(buildingId);
     }
 
+    // Funktion zum Löschen des Freitexts
+    function deleteTextValue(buildingId) {
+        const textData = getLocalStorageTextData();
+        delete textData[buildingId];
+        setLocalStorageTextData(textData);
+        updateTextInputField(buildingId);
+    }
+
     // Erstellen eines DIV-Elements für die Benutzeroberfläche
     const personalDiv = document.createElement('div');
     personalDiv.innerHTML = `
         <input id="personalCountInput" type="number" placeholder="Personalzahl">
+        <input id="personalTextInput" type="text" placeholder="Freitext">
         <button id="confirmButton" class="btn btn-xs btn-success">Bestätigen</button>
         <button id="deleteButton" class="btn btn-xs btn-danger">-</button>
         <button id="showTableButton" class="btn btn-default btn-xs">Zeige Tabelle</button>
@@ -59,6 +93,7 @@
 
     // Abfragen von DOM-Elementen
     const inputField = document.getElementById('personalCountInput');
+    const textInputField = document.getElementById('personalTextInput');
     const confirmButton = document.getElementById('confirmButton');
     const deleteButton = document.getElementById('deleteButton');
     const showTableButton = document.getElementById('showTableButton');
@@ -66,19 +101,23 @@
     // Extrahieren der Gebäude-ID aus der aktuellen URL
     const buildingId = window.location.pathname.split('/').pop();
     updateInputField(buildingId);
+    updateTextInputField(buildingId);
 
     // Event-Listener für den "Bestätigen"-Button
     confirmButton.addEventListener('click', function() {
         const personalCount = parseInt(inputField.value);
+        const textValue = textInputField.value;
         updatePersonalCount(buildingId, personalCount);
-        alert(`Personalzahl ${personalCount} für Gebäude ${buildingId} gespeichert!`);
+        updateTextValue(buildingId, textValue);
+        alert(`Personalzahl ${personalCount} und Freitext "${textValue}" für Gebäude ${buildingId} gespeichert!`);
     });
 
     // Event-Listener für den "Löschen"-Button
     deleteButton.addEventListener('click', function() {
-        if (confirm('Möchten Sie diese Personalzahl löschen?')) {
+        if (confirm('Möchten Sie diese Personalzahl und den Freitext löschen?')) {
             deletePersonalCount(buildingId);
-            alert(`Personalzahl für Gebäude ${buildingId} gelöscht!`);
+            deleteTextValue(buildingId);
+            alert(`Personalzahl und Freitext für Gebäude ${buildingId} gelöscht!`);
         }
     });
 
@@ -100,43 +139,35 @@
                 <th>Name</th>
                 <th>Soll Personal</th>
                 <th>Ist Personal</th>
+                <th>Freitext</th>
             </tr>
         `;
 
         const personalData = getLocalStorageData();
+        const textData = getLocalStorageTextData();
 
         // Iterieren durch die gespeicherten Personaldaten und Gebäudeinformationen
-        for (const buildingId in personalData) {
-            const buildingInfo = buildingsData.find(building => building.id === parseInt(buildingId));
-            if (buildingInfo) {
-                const row = document.createElement('tr');
-                row.innerHTML = `
-                    <td>${buildingId}</td>
-                    <td><a href="https://www.leitstellenspiel.de/buildings/${buildingId}" target="_blank">${buildingInfo.caption}</a></td>
-                    <td>${personalData[buildingId]}</td>
-                    <td>${buildingInfo.personal_count}</td>
-                    <td><button class="btn btn-xs btn-danger delete-button">-</button></td>
-                `;
+    for (const buildingId in personalData) {
+        const buildingInfo = buildingsData.find(building => building.id === parseInt(buildingId));
+        const textInfo = textData[buildingId] || ''; // Freitext für diese Paarung
+        if (buildingInfo) {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${buildingId}</td>
+                <td><a href="https://www.leitstellenspiel.de/buildings/${buildingId}" target="_blank">${buildingInfo.caption}</a></td>
+                <td>${personalData[buildingId]}</td>
+                <td>${buildingInfo.personal_count}</td>
+                <td>${textInfo}</td>
+            `;
 
-                // Überprüfen, ob der PersonalCount größer als das Ist Personal ist
-                if (personalData[buildingId] < buildingInfo.personal_count) {
-                    row.style.backgroundColor = 'green'; // Hintergrundfarbe grün setzen
-                }
-
-                // Hinzufügen eines Event Listeners für den Löschen-Button
-                const deleteButton = row.querySelector('.delete-button');
-                deleteButton.addEventListener('click', function() {
-                    //if (confirm('Möchten Sie diese Personalzahl löschen?')) {
-                        deletePersonalCount(buildingId);
-                        //alert(`Personalzahl für Gebäude ${buildingId} gelöscht!`);
-                        // Aktualisiere die Tabelle, nachdem ein Eintrag gelöscht wurde
-                        showBuildingTable();
-                    //}
-                });
-
-                table.appendChild(row);
+            // Überprüfen, ob der PersonalCount kleiner als das Ist Personal ist und die Zeile grün einfärben
+            if (personalData[buildingId] < buildingInfo.personal_count) {
+                row.style.backgroundColor = 'green'; // Hintergrundfarbe grün setzen
             }
+
+            table.appendChild(row);
         }
+    }
 
         // Öffnen eines Popup-Fensters und Anhängen der Tabelle
         const popup = window.open('', '_blank');
